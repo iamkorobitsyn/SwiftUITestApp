4d363729954a56461d623159c8dc467da2c38145
iamkorobitsyn <iamkorobitsyn@icloud.com>
2024-06-18 01:58:15 +0300

    create_tabView

--- SwiftUITestApp/ContentView.swift
+++ SwiftUITestApp/ContentView.swift
@@ -6,15 +6,56 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-
-    var body: some View {
-        LinearGradient(colors: [.red, .green], startPoint: .top, endPoint: .bottom)
+    
+    @State private var selected = 0
+    
+    init() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.darkGray
+        
+        appearance.stackedLayoutAppearance.selected.iconColor = UIColor.orange
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.orange]
+        appearance.stackedLayoutAppearance.normal.iconColor = .systemGray4
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.systemGray4]
+        
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
     }
 
+    var body: some View {
+
+            
+        TabView(selection: $selected) {
+            Text("FirstView")
+                .font(.system(size: 30))
+                .tabItem {
+                    
+                    Image("RandomizerViewIcon")
+                        .frame(alignment: .center)
+                    Text(selected == 0 ? "___" : "_")   
+                }
+                .tag(0)
+            
+            Text("SecondView")
+                .tabItem {
+                    Image("PersonListViewIcon")
+                        .frame(alignment: .center)
+                    Text(selected == 1 ? "___" : "_")
+                }
+                .tag(1)
+        }
+    }
 }
+
+//struct MainScreen: View {
+//    
+//    var body: some View {
+//        LinearGradient(colors: [.red, .green], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+//    }
+//}
 
 #Preview {
     ContentView()
